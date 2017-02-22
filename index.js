12f6c01c94681931f8caa323550d6f16faa642d6
luke <nerorevenge@gmail.com>
2017-02-22 18:57:00 -0500

    Deletion of file and recreation of file scenarios handled

--- index.js
+++ index.js
@@ -10,10 +10,17 @@ function createFileReadStream(fileName, start, end) {
     }), 'data');
 }
 
+// don't think I need this needs to be put into queue for considered for deletion
+function val_checkForFileDeletion_stream(fileName) {
+    return k.fromEvents(fs.watch(fileName), 'change')
+        .map(x => fs.existsSync(fileName))
+        .skipWhile(x => x == true)
+}
+
 
 // String -> Stream Observables
 // creates a stream of readstreams
-function valKefir(fileName) {
+function val_fileContentChange_stream(fileName) {
     const initialFileSize = fs.statSync(fileName).size;
     const init = {
         seekFrom: 0,
@@ -33,15 +40,34 @@ function valKefir(fileName) {
         .map(x => createFileReadStream(fileName, x.seekFrom, x.size));
 }
 
-// needs to be refactored
-function val(fileName, callback) {
-    return valKefir(fileName).onValue(function(x) {
-        x.scan((x, y) => x + y, "")
-            .onValue(callback);
-    })
+
+// don't think I really need this .. needs to be considered for deletion
+function val_mergedStream(fileName) {
+    return val_fileContentChange_stream(fileName)
+        .takeUntilBy(val_checkForFileDeletion_stream(fileName));
 }
 
-//val("chicken.txt", console.log);
+function val_pollForFileName_stream(fileName, interval) {
+    return k.fromPoll(interval, () => fs.existsSync(fileName))
+        .takeWhile(x => x == false)
+}
+
+
+// 
+function val(fileName, callback, interval) {
+    val_mergedStream(fileName)
+        .onValue(x => {
+            x.scan((x, y) => x + y, "")
+                .onValue(callback);
+        })
+        .onEnd(function(x) {
+            console.log("I got deleted")
+	    val_pollForFileName_stream(fileName, interval)
+	       .onEnd(x => val(fileName, callback, interval));
+        })
+}
+
+//val("chicken.txt", console.log, 1000);
 //
 
 module.exports = {
